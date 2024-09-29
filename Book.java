88097aca196f899796061d980a6645168ef955b2
BollumallaSowmya <23wh1a6623@bvrithyderabad.edu.in>
2024-09-29 22:52:27 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,60 +1,28 @@
-public class Book {
-    String bookName;
-    String isbnNumber;
-    String authorName;
-    String publisher;
+    public class Book {
+    private String name;
+    private int isbn;
+    private String author;
+    private String pub;
 
-    public Book(String bookName, String isbnNumber, String authorName, String publisher) {
-        this.bookName = bookName;
-        this.isbnNumber = isbnNumber;
-        this.authorName = authorName;
-        this.publisher = publisher;
+   
+    public Book(String name, int isbn,String author,String pub) {
+        this.name = name;
+        this.isbn=isbn;
+        this.author = author;
+        this.pub=pub;
     }
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public void setIsbnNumber(String isbnNumber) {
-        this.isbnNumber = isbnNumber;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public String getIsbnNumber() {
-        return isbnNumber;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
+   
     public String getBookInfo() {
-        return "Book Name: " + bookName + "\n" +
-               "ISBN Number: " + isbnNumber + "\n" +
-               "Author Name: " + authorName + "\n" +
-               "Publisher: " + publisher;
+      
+        return "Book Name: " + name + " , isbn: " + isbn +  ",Author: " + author  + ",pub :  " + pub ;
     }
 
     public static void main(String[] args) {
-        Book book1 = new Book("Java", "978", "Bloch", "Wesley");
-        System.out.println(book1.getBookInfo());
-        book1.setBookName("Java (2nd Edition)");
-        System.out.println("\nUpdated Book Information:");
+
+        Book book1 = new Book("Effective Java" ,123,"Joshua Bloch","akshi");
+
+      
         System.out.println(book1.getBookInfo());
     }
 }
