fa16c8232ad53b21c7d1ef972cc39719cee93e84
BollumallaSowmya <23wh1a6623@bvrithyderabad.edu.in>
2024-09-29 23:12:09 +0530

    Update merge.java

--- merge.java
+++ merge.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
-
-public class MergeStrings {
-    static String mergeStrings(String s1, String s2) {
-        String merged = "";
-        int i = 0, j = 0;
-        while (i < s1.length() || j < s2.length()) {
-            if (i < s1.length()) {
-                merged += s1.charAt(i++);
-            }
-            if (j < s2.length()) {
-                merged += s2.charAt(j++);
-            }
+public class StringMerge{
+    public static String mergeStrings(String s1 , String s2)
+    {
+        StringBuilder mergeString = new StringBuilder();
+        int i=0,j=0;
+        while(i<s1.length() && j<s2.length() ){
+            mergedString.append(s1.charAt(i)).append(s2.charAt(j)));
+            i++;
+            j++;
         }
-
-        return merged;
+        if(i<s1.length()){
+            mergedString.append(s1.substring(i));
+        }
+        if(j<s2.length()){
+            mergedString.append(s2.substring(j));
+        }
+        return mergedString.tostring();
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first string (s1):");
-        String s1 = scanner.nextLine();
-        System.out.println("Enter second string (s2):");
-        String s2 = scanner.nextLine();
-        System.out.println("Merged string: " + mergeStrings(s1, s2));
+    public static void main(String[] args){
+        String s1="abc";
+        String s2="def";
+        String mergedString = mergeString(s1,s2);
+        System.out.println(mergedString);
     }
 }
